e5ed62f5120b5e31cd621df64b643981c20b58ee
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-08-15 21:34:10 -0500

    Laid out more JSX files and set up Tailwind CSS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+return (
+    <header className="footer">
+        <h1>Welcome</h1>
+    </header>
+);
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header className="header" style={styles.headerStyle}>
+        <h1>Welcome</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
