d35581527de7b99fe1c9333133db68ad3654dc8d
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-08-20 14:55:05 -0500

    Added favicon and Edited Card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ return (
             <img className="w-full" src={image} alt={title} />
             <div className="flex flex-col justify-between p-4 text-white">
                 <h2 className="mb-2 text-2xl font-bold">{title}</h2>
-                <p className="mb-4 text-sm font-medium">{description}</p>
+                <p className="mb-4 text-medium font-medium">{description}</p>
                 <button>
                     <a href={githubUrl} className="m-2">GitHub</a>
                 </button>
