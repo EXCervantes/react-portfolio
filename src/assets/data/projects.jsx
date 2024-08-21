fa9851963f5c8032c9ff675db5a675b82e808c39
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-08-20 22:22:25 -0500

    Added more projects, Resume PDF, and edited tailwind config

--- src/assets/data/projects.jsx
+++ src/assets/data/projects.jsx
@@ -2,7 +2,8 @@ import image1 from "../project1.jpg";
 import image2 from "../project2.jpg";
 import image3 from "../project3.jpg";
 import image4 from "../project4.jpg";
-
+import image5 from "../project5.jpg";
+import image6 from "../project6.jpg";
 
 export const projects = [
     {
@@ -32,5 +33,19 @@ export const projects = [
         description: 'This is a little bot for the Telegram app to fetch the URLs for GIFs and videos found on the X/Twitter platform. Then it sends them to the user as a link where a user can choose to view it or download if they desire.',
         deployedUrl: 'https://taskattacker.onrender.com/',
         githubUrl: 'https://github.com/NoahCalderwood/TaskAttacker'
+    },
+    {
+        image: image5,
+        title: 'J.A.T.E',
+        description: 'Takes in text input and stores it either in local storage or IndexDB. This is a PWA complete with a page manifest and service workers. It can be used offline.',
+        deployedUrl: 'https://pwa-jate-zgyl.onrender.com/',
+        githubUrl: 'https://github.com/EXCervantes/pwa-jate'
+    },
+    {
+        image: image6,
+        title: 'SVG Logo Maker',
+        description: 'This is an application used to create SVG files via prompts in the terminal from three different shapes and any color including HEX values.',
+        deployedUrl: 'https://github.com/EXCervantes/svg-logo-maker',
+        githubUrl: 'https://github.com/EXCervantes/svg-logo-maker'
     },
 ];
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,15 +1,15 @@
 import { frontEndSkills, backEndSkills} from "../assets/data/resumeDetails"
-import { Button } from "flowbite-react";
-// import resumePDF from ".."
+import resumePDF from "../assets/docs/Edwardo-Cervantes-Resume.pdf"
 
 const Resume = () => {
 return (
     <div className="container mx-auto mb-20" id="resume">
-        <h2 className="mb-12 mt-20 text-center text-4xl">Download Resume
+        <h2 className="mb-12 mt-20 text-center text-4xl">Resume
         </h2>
-        <div>
-        <button className="flex flex-wrap gap-2">
-    {/* TODO make Resume and setup download button */}
+        <div className="flex flex-wrap justify-center gap-2">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" >
+        <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+        <a  href={resumePDF} download><span>Download Resume</span></a>
         </button>
         </div>
         <h2 className="mb-12 mt-20 text-center text-4xl">Front End Proficiencies
@@ -22,7 +22,7 @@ return (
                         {frontend.name}
                         </h3>
                     </div>
-                    <div className="text-md border-b-2 border-yellow-400 font-semibold lg:text-xl">
+                    <div className="text-md font-semibold lg:text-xl">
                         <span>{frontend.experience}</span>
                     </div>
                     </div>
@@ -38,7 +38,7 @@ return (
                         {backend.name}
                         </h3>
                     </div>
-                    <div className="text-md border-b-2 border-yellow-400 font-semibold lg:text-xl">
+                    <div className="text-md font-semibold lg:text-xl">
                         <span>{backend.experience}</span>
                     </div>
                     </div>
