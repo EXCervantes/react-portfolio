8357e4ae9372e6215ee0b6c7e8dcc07a5d2aa2bf
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-08-21 22:31:02 -0500

    Styling edit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -93,7 +93,9 @@ const Contact = () => {
                     <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type="submit" >
                     Submit
                     </button>
-                    <div><p>{error || successMessage}</p></div>
+                        <div className="pt-4">
+                            <p>{error || successMessage}</p>
+                        </div>
                 </div>
             </form>
         </div>
